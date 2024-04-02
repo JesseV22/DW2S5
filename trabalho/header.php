f419f3542c3395662f7991957ee74d4848986abd
JesseV2 <starkjesse2020@gmail.com>
2024-04-02 20:52:37 -0300

    ICON NA PAGINA

--- trabalho/header.php
+++ trabalho/header.php
@@ -1,6 +1,7 @@
 <?php
 // Função para registrar o acesso à página atual
-function registrarAcesso($pagina) {
+function registrarAcesso($pagina)
+{
     // Define o nome do arquivo de log
     $logFile = "logs/{$pagina}_log.txt";
 
@@ -34,13 +35,14 @@ registrarAcesso(basename($_SERVER['PHP_SELF'], '.php'));
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" type="logo.png" href="caminho/para/seu/icone.png">
+    <link rel="shortcut icon" href="logo.ico" type="image/x-icon">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
+    <link
+        href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
         rel="stylesheet">
     <style>
         body {
@@ -51,24 +53,25 @@ registrarAcesso(basename($_SERVER['PHP_SELF'], '.php'));
             /* Fonte do texto */
         }
 
-        body::before {
-            content: "";
-            position: absolute;
+        #video-background {
+            position: fixed;
             top: 0;
             left: 0;
             width: 100%;
             height: 100%;
-            background-image: url('https://www.modalmais.com.br/wp-content/uploads/2021/04/iStock-801479766-3.jpg');
-            /* URL da sua imagem de fundo */
-            background-size: cover;
-            /* Ajusta o tamanho da imagem para cobrir todo o corpo */
-            filter: blur(5px);
-            /* Ajuste o valor do raio do desfoque conforme necessário */
             z-index: -1;
-            /* Coloca o fundo desfocado atrás do conteúdo */
+            filter: blur(5px); /* Adiciona o efeito de desfoque */
+            background-color: rgba(0, 0, 0, 0.5); /* Cor semi-transparente escura */
+
         }
 
+        video {
+            width: 100%;
+            height: 100%;
+            object-fit: cover; /* Faz com que o vídeo cubra completamente o espaço do contêiner */
+        }
 
+      
 
         .img-lorem {
             width: 30%;
@@ -90,10 +93,18 @@ registrarAcesso(basename($_SERVER['PHP_SELF'], '.php'));
 </head>
 
 <body>
+<div id="video-background">
+        <video autoplay muted loop>
+            <source src="markt.mp4" type="video/mp4">
+            <!-- Adicione outros formatos de vídeo aqui se desejar suporte a navegadores diferentes -->
+            Desculpe, seu navegador não suporta vídeos HTML5.
+        </video>
+    </div>
+
 
     <svg xmlns="" style="display: none;">
         <symbol id="bootstrap" viewBox="0 0 118 94">
-            <title>Bootstrap</title>
+            <title>Jesse Capital</title>
             <path fill-rule="evenodd" clip-rule="evenodd"
                 d="M24.509 0c-6.733 0-11.715 5.893-11.492 12.284.214 6.14-.064 14.092-2.066 20.577C8.943 39.365 5.547 43.485 0 44.014v5.972c5.547.529 8.943 4.649 10.951 11.153 2.002 6.485 2.28 14.437 2.066 20.577C12.794 88.106 17.776 94 24.51 94H93.5c6.733 0 11.714-5.893 11.491-12.284-.214-6.14.064-14.092 2.066-20.577 2.009-6.504 5.396-10.624 10.943-11.153v-5.972c-5.547-.529-8.934-4.649-10.943-11.153-2.002-6.484-2.28-14.437-2.066-20.577C105.214 5.894 100.233 0 93.5 0H24.508zM80 57.863C80 66.663 73.436 72 62.543 72H44a2 2 0 01-2-2V24a2 2 0 012-2h18.437c9.083 0 15.044 4.92 15.044 12.474 0 5.302-4.01 10.049-9.119 10.88v.277C75.317 46.394 80 51.21 80 57.863zM60.521 28.34H49.948v14.934h8.905c6.884 0 10.68-2.772 10.68-7.727 0-4.643-3.264-7.207-9.012-7.207zM49.948 49.2v16.458H60.91c7.167 0 10.964-2.876 10.964-8.281 0-5.406-3.903-8.178-11.425-8.178H49.948z">
             </path>
@@ -101,10 +112,10 @@ registrarAcesso(basename($_SERVER['PHP_SELF'], '.php'));
     </svg>
 
     <div class="container">
-        
+
         <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
             <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
-                
+
                 <h2>J.V.O Capital </h2>
 
             </a>
--- trabalho/index.php
+++ trabalho/index.php
@@ -3,9 +3,6 @@
 include ('header.php');
 ?>
 
-
-
-
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link
@@ -25,48 +22,141 @@ include ('header.php');
         font-weight: 700;
         font-style: normal;
     }
+    .custom-style {
+    padding: 1rem; /* Mesmo que p-4 */
+    margin-bottom: 1rem; /* Mesmo que mb-4 */
+    border-radius: 0.25rem; /* Mesmo que rounded */
+    /* Você pode adicionar outros estilos aqui */
+}
+
+
+    .tradingview-widget-container {
+        position: relative;
+        width: 100%;
+        max-width: 800px;
+        /* Defina a largura máxima conforme necessário */
+        margin: 0 auto;
+        /* Centraliza o widget */
+    }
+
+    .tradingview-widget-container__widget {
+        width: 100%;
+    }
+
+    /* Estilo adicional para tornar a responsividade do widget agradável */
+    .tradingview-widget-container iframe {
+        max-width: 100%;
+        height: auto;
+    }
+
+    /* Adicionar regras de mídia queries para dispositivos móveis */
+    @media only screen and (max-width: 768px) {
+        .tradingview-widget-container {
+            max-width: 100%;
+            /* Altera a largura máxima para 100% em telas menores que 768px */
+        }
+    }
 </style>
+
 <main>
 
-<div class="inicio">
-    <div class="bg-light p-4 mb-4 rounded">
-        <h1 class="text-center">Jessé Vitorino Oliveira Capital </h1>
-    </div>
+   
+        <div class="custom-style">
+            <h1 class="text-center">Jessé Vitorino Oliveira Capital </h1>
+        </div>
+    
 
     <img class="img-lorem img-thumbnail m-4 rounded float-end" src="logo.png" alt="Investiments">
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
 
-    <span>Nós somos investidores impulsionados por pesquisas constantes, focados em oportunidades especiais, guiados por
+    <br><br><br><br><br> <br><br> <br><br><br><br><br>
+
+    <span>Nós somos investidores impulsionados por pesquisas constantes, focados em oportunidades especiais, guiados
+        por
         times com incentivos alinhados e perspectivas sólidas de crescimento a longo prazo </span>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
+    <br><br><br><br><br>
 
 
-</div>
+    <!-- TradingView Widget BEGIN -->
+    <div class="tradingview-widget-container">
+        <div class="tradingview-widget-container__widget"></div>
+        <div class="tradingview-widget-copyright"><a href="https://br.tradingview.com/" rel="noopener nofollow"
+                target="_blank"><span class="blue-text">Monitore todos os mercados no TradingView</span></a></div>
+        <script type="text/javascript"
+            src="https://s3.tradingview.com/external-embedding/embed-widget-symbol-overview.js" async>
+                {
+                    "symbols": [
+                        [
+                            "BMFBOVESPA:IBOV|1D"
+                        ],
+                        [
+                            "TVC:DXY|1D"
+                        ],
+                        [
+                            "SP:SPX|1D"
+                        ],
+                        [
+                            "CRYPTO:BTCUSD|1D"
+                        ],
+                        [
+                            "BITSTAMP:ETHUSD|1D"
+                        ],
+                        [
+                            "NASDAQ:META|1D"
+                        ],
+                        [
+                            "NASDAQ:NVDA|1D"
+                        ],
+                        [
+                            "NASDAQ:AMZN|1D"
+                        ],
+                        [
+                            "ECONOMICS:BRM2|1D"
+                        ],
+                        [
+                            "COMEX:GC1!|1D"
+                        ]
+                    ],
+                        "chartOnly": false,
+                            "width": 550,
+                                "height": 400,
+                                    "locale": "br",
+                                        "colorTheme": "light",
+                                            "autosize": false,
+                                                "showVolume": false,
+                                                    "showMA": false,
+                                                        "hideDateRanges": false,
+                                                            "hideMarketStatus": false,
+                                                                "hideSymbolLogo": false,
+                                                                    "scalePosition": "right",
+                                                                        "scaleMode": "Normal",
+                                                                            "fontFamily": "-apple-system, BlinkMacSystemFont, Trebuchet MS, Roboto, Ubuntu, sans-serif",
+                                                                                "fontSize": "10",
+                                                                                    "noTimeScale": false,
+                                                                                        "valuesTracking": "1",
+                                                                                            "changeMode": "price-and-percent",
+                                                                                                "chartType": "area",
+                                                                                                    "maLineColor": "#2962FF",
+                                                                                                        "maLineWidth": 1,
+                                                                                                            "maLength": 9,
+                                                                                                                "lineWidth": 2,
+                                                                                                                    "lineType": 0,
+                                                                                                                        "dateRanges": [
+                                                                                                                            "1d|1",
+                                                                                                                            "1m|30",
+                                                                                                                            "3m|60",
+                                                                                                                            "12m|1D",
+                                                                                                                            "60m|1W",
+                                                                                                                            "all|1M"
+                                                                                                                        ]
+                }
+            </script>
+    </div>
+    <!-- TradingView Widget END -->
+    <br><br><br><br><br> <br><br><br><br><br>
+
+    </div>
+
 </main>
-
 
 <?php
 // Aqui você incluirá o footer.php
