f1a1a446a6ee61d2b9474f9b3eeceb4be9531682
JesseV2 <starkjesse2020@gmail.com>
2024-04-02 16:57:02 -0300

    remove

--- dw2s5-main/Aula6/Praticando1/menu.php
+++
@@ -1,6 +0,0 @@
-<ul class="nav nav-pills">
-                <li class="nav-item"><a href="index.php" class="nav-link active" aria-current="page">Início</a></li>
-                <li class="nav-item"><a href="spiderman.php" class="nav-link">Spiderman</a></li>
-                <li class="nav-item"><a href="dodge.php" class="nav-link">Dodge</a></li>
-                <li class="nav-item"><a href="form.php" class="nav-link">Contato</a></li>
-</ul>
